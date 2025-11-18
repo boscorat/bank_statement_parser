355807200f6e6691dc4a0c10d3ee8a6743dd6ee3
Jason Farrar <stillwaters.technology@gmail.com>
2025-11-18 12:07:56 +0000

    Simple Excel Export and Batch Filtering of reports added

--- src/bank_statement_parser/modules/classes/reports.py
+++ src/bank_statement_parser/modules/classes/reports.py
@@ -6,64 +6,116 @@ from xlsxwriter import Workbook
 import bank_statement_parser.modules.paths as pt
 
 
-def export_excel(path: Path):
+def export_excel(path: Path, type: str = "full", ID_BATCH: str | None = None):  # type can be full or simple
     with Workbook(str(path)) as wb:
-        DimStatement().all.write_excel(
-            workbook=wb,
-            worksheet="statement",
-            autofit=False,
-            table_name="statement",
-            table_style="Table Style Medium 4",
-        )
-        DimAccount().all.write_excel(
-            workbook=wb,
-            worksheet="account",
-            autofit=False,
-            table_name="account",
-            table_style="Table Style Medium 4",
-        )
-        DimTime().all.write_excel(
-            workbook=wb,
-            worksheet="calendar",
-            autofit=False,
-            table_name="calendar",
-            table_style="Table Style Medium 4",
-        )
-        FactTransaction().all.write_excel(
-            workbook=wb,
-            worksheet="transactions",
-            autofit=False,
-            table_name="transactions",
-            table_style="Table Style Medium 4",
-            float_precision=2,
-        )
-        FactBalance().all.write_excel(
-            workbook=wb, worksheet="balances", autofit=False, table_name="balances", table_style="Table Style Medium 4", float_precision=2
-        )
-        GapReport().all.write_excel(
-            workbook=wb,
-            worksheet="gaps",
-            autofit=False,
-            table_name="gaps",
-            table_style="Table Style Medium 4",
-        )
+        if type == "full":
+            DimStatement(ID_BATCH).all.collect().write_excel(
+                workbook=wb,
+                worksheet="statement",
+                autofit=False,
+                table_name="statement",
+                table_style="Table Style Medium 4",
+            )
+            DimAccount(ID_BATCH).all.collect().write_excel(
+                workbook=wb,
+                worksheet="account",
+                autofit=False,
+                table_name="account",
+                table_style="Table Style Medium 4",
+            )
+            DimTime(ID_BATCH).all.collect().write_excel(
+                workbook=wb,
+                worksheet="calendar",
+                autofit=False,
+                table_name="calendar",
+                table_style="Table Style Medium 4",
+            )
+            FactTransaction(ID_BATCH).all.collect().write_excel(
+                workbook=wb,
+                worksheet="transactions",
+                autofit=False,
+                table_name="transactions",
+                table_style="Table Style Medium 4",
+                float_precision=2,
+            )
+            FactBalance(ID_BATCH).all.collect().write_excel(
+                workbook=wb,
+                worksheet="balances",
+                autofit=False,
+                table_name="balances",
+                table_style="Table Style Medium 4",
+                float_precision=2,
+            )
+            GapReport().all.collect().write_excel(
+                workbook=wb,
+                worksheet="gaps",
+                autofit=False,
+                table_name="gaps",
+                table_style="Table Style Medium 4",
+            )
+            FlatTransaction(ID_BATCH).all.collect().write_excel(
+                workbook=wb,
+                worksheet="flat",
+                autofit=False,
+                table_name="flat",
+                table_style="Table Style Medium 4",
+            )
+        elif type == "simple":
+            FlatTransaction(ID_BATCH).all.collect().write_excel(
+                workbook=wb,
+                worksheet="transactions",
+                autofit=False,
+                table_name="flat",
+                table_style="Table Style Medium 4",
+            )
 
 
 def main():
-    export_excel(pt.EXCEL.joinpath("test.xlsx"))
+    export_excel(pt.EXCEL.joinpath("test.xlsx"), type="simple", ID_BATCH="d74e768f-94a8-4c54-9265-870e3b3c392c")
     # print(pl.read_parquet(pt.STATEMENT_LINES))
 
 
-class FactBalance:
-    def __init__(self) -> None:
-        self._cartesian_date_account: pl.DataFrame = (
-            DimTime().all.select("id_date").join(pl.read_parquet(pt.STATEMENT_HEADS).select(id_account="ID_ACCOUNT").unique(), how="cross")
+class FlatTransaction:
+    def __init__(self, ID_BATCH: str | None = None) -> None:
+        self.base: pl.LazyFrame = (
+            FactTransaction(ID_BATCH)
+            .all.join(DimTime().all, on="id_date", how="inner")
+            .join(DimAccount().all, on="id_account", how="inner")
+            .join(DimStatement().all, on="id_statement", how="inner")
         )
-        self._account_days: pl.DataFrame = (
-            pl.read_parquet(pt.STATEMENT_HEADS)
-            .select(id_statement="ID_STATEMENT", id_account="ID_ACCOUNT")
+        self.all: pl.LazyFrame = self.base.select(
+            pl.col("id_date").alias("transaction_date"),
+            "statement_date",
+            "filename",
+            "company",
+            "account_type",
+            "account_number",
+            "account_holder",
+            "transaction_number",
+            pl.col("transaction_credit_or_debit").alias("CD"),
+            pl.col("transaction_type").alias("type"),
+            "transaction_desc",
+            pl.col("transaction_desc").str.head(25).alias("short_desc"),
+            "value_in",
+            "value_out",
+            "value",
+        )
+
+
+class FactBalance:
+    def __init__(self, ID_BATCH: str | None = None) -> None:
+        self.__heads: pl.LazyFrame = (
+            pl.read_parquet(pt.STATEMENT_HEADS).filter((pl.lit(ID_BATCH).is_null()) | (pl.col("ID_BATCH") == pl.lit(ID_BATCH))).lazy()
+        )
+        self._cartesian_date_account: pl.LazyFrame = (
+            DimTime(ID_BATCH=ID_BATCH).all.select("id_date").join(self.__heads.select(id_account="ID_ACCOUNT").unique().lazy(), how="cross")
+        )
+        self._account_days: pl.LazyFrame = (
+            self.__heads.select(id_statement="ID_STATEMENT", id_account="ID_ACCOUNT")
             .join(
-                pl.read_parquet(pt.STATEMENT_LINES).select(
+                pl.read_parquet(pt.STATEMENT_LINES)
+                .lazy()
+                .select(
                     id_statement="ID_STATEMENT",
                     id_date="STD_TRANSACTION_DATE",
                     trnno="STD_TRANSACTION_NUMBER",
@@ -83,16 +135,16 @@ class FactBalance:
             )
             .sort("id_account", "id_date")
         )
-        self._bookends: pl.DataFrame = self._account_days.group_by("id_account").agg(
+        self._bookends: pl.LazyFrame = self._account_days.group_by("id_account").agg(
             first_day=pl.col("id_date").min(), last_day=pl.col("id_date").max()
         )
-        self.raw: pl.DataFrame = self._cartesian_date_account.join(self._bookends, how="inner", on="id_account", validate="m:1").join(
+        self.raw: pl.LazyFrame = self._cartesian_date_account.join(self._bookends, how="inner", on="id_account", validate="m:1").join(
             self._account_days, on=["id_account", "id_date"], how="left", validate="1:1"
         )
-        self.all: pl.DataFrame = pl.DataFrame()
+        self.__all: pl.DataFrame = pl.DataFrame()
 
-        for acct in self.raw.partition_by("id_account"):
-            self.all.vstack(
+        for acct in self.raw.collect().partition_by("id_account"):
+            self.__all.vstack(
                 acct.sort("id_date")
                 .with_columns(
                     opening_balance=pl.col("closing_balance")
@@ -110,26 +162,36 @@ class FactBalance:
                 .drop("movement"),
                 in_place=True,
             )
+        self.all = self.__all.lazy()
 
 
 class DimTime:
-    def __init__(self) -> None:
-        self.start_date: str = pl.read_parquet(pt.STATEMENT_LINES).select("STD_TRANSACTION_DATE").min().item()
-        self.end_date: str = pl.read_parquet(pt.STATEMENT_HEADS).select("STD_STATEMENT_DATE").max().item()
+    def __init__(self, ID_BATCH: str | None = None) -> None:
+        self.__heads: pl.DataFrame = pl.read_parquet(pt.STATEMENT_HEADS).filter(
+            (pl.lit(ID_BATCH).is_null()) | (pl.col("ID_BATCH") == pl.lit(ID_BATCH))
+        )
+        self.end_date: str = self.__heads.select("STD_STATEMENT_DATE").max().item()
+        self.start_date: str = (
+            pl.read_parquet(pt.STATEMENT_LINES)
+            .join(self.__heads, on="ID_STATEMENT", how="semi")
+            .select("STD_TRANSACTION_DATE")
+            .min()
+            .item()
+        )
         self.date_series: pl.Series = pl.Series("ID_DATE", pl.date_ranges(self.start_date, self.end_date, eager=True)).explode()
-        self.all: pl.DataFrame = pl.DataFrame(self.date_series).select(
+        self.all: pl.LazyFrame = pl.LazyFrame(self.date_series).select(
             id_date="ID_DATE",
             date_local_format=pl.col("ID_DATE").dt.to_string("%x"),
             date_integer=pl.col("ID_DATE").dt.year().mul(100).add(pl.col("ID_DATE").dt.month()).mul(100).add(pl.col("ID_DATE").dt.day()),
             year=pl.col("ID_DATE").dt.year(),
-            year_short=pl.col("ID_DATE").dt.to_string("%y"),
+            year_short=pl.col("ID_DATE").dt.to_string("%y").cast(int),
             quarter=pl.col("ID_DATE").dt.quarter(),
             quarter_name=pl.lit("Q").add(pl.col("ID_DATE").dt.quarter().cast(str)),
             month_number=pl.col("ID_DATE").dt.month(),
             month_number_padded=pl.col("ID_DATE").dt.to_string("%m"),
             month_name=pl.col("ID_DATE").dt.to_string("%B"),
             month_abbrv=pl.col("ID_DATE").dt.to_string("%b"),
-            period=pl.col("ID_DATE").dt.year().mul(100).add(pl.col("ID_DATE").dt.month()),
+            period=pl.col("ID_DATE").dt.year().mul(100).add(pl.col("ID_DATE").dt.month()).cast(int),
             week=pl.col("ID_DATE").dt.week(),
             year_week=pl.col("ID_DATE").dt.year().mul(100).add(pl.col("ID_DATE").dt.week()),
             day_of_month=pl.col("ID_DATE").dt.day(),
@@ -156,19 +218,16 @@ class DimTime:
 
 
 class DimStatement:
-    def __init__(self) -> None:
-        self.raw: pl.DataFrame = pl.read_parquet(pt.STATEMENT_HEADS).join(
-            other=pl.read_parquet(pt.BATCH_LINES), on=["ID_STATEMENT", "ID_BATCH"], how="inner", validate="1:1", coalesce=True
+    def __init__(self, ID_BATCH: str | None = None) -> None:
+        self.__heads: pl.LazyFrame = (
+            pl.read_parquet(pt.STATEMENT_HEADS).filter((pl.lit(ID_BATCH).is_null()) | (pl.col("ID_BATCH") == pl.lit(ID_BATCH))).lazy()
         )
-        self.all: pl.DataFrame = self.raw.select(
+        self.raw: pl.LazyFrame = self.__heads.join(
+            other=pl.read_parquet(pt.BATCH_LINES).lazy(), on=["ID_STATEMENT", "ID_BATCH"], how="inner", validate="1:1", coalesce=True
+        )
+        self.all: pl.LazyFrame = self.raw.select(
             id_statement="ID_STATEMENT",
-            # id_account="ID_ACCOUNT",
             id_statement_int="index",
-            # company="STD_COMPANY",
-            # account_type="STD_ACCOUNT_NUMBER",
-            # account="STD_ACCOUNT",
-            # account_number="STD_ACCOUNT_NUMBER",
-            # account_holder="STD_ACCOUNT_HOLDER",
             statement_date="STD_STATEMENT_DATE",
             filename="STD_FILENAME",
             batch_time="STD_UPDATETIME",
@@ -176,23 +235,20 @@ class DimStatement:
 
 
 class DimAccount:
-    def __init__(self) -> None:
-        self.raw: pl.DataFrame = pl.read_parquet(pt.STATEMENT_HEADS).join(
-            other=pl.read_parquet(pt.BATCH_LINES), on=["ID_STATEMENT", "ID_BATCH"], how="inner", validate="1:1", coalesce=True
+    def __init__(self, ID_BATCH: str | None = None) -> None:
+        self.__heads: pl.LazyFrame = (
+            pl.read_parquet(pt.STATEMENT_HEADS).filter((pl.lit(ID_BATCH).is_null()) | (pl.col("ID_BATCH") == pl.lit(ID_BATCH))).lazy()
         )
-        self.all: pl.DataFrame = (
+        self.raw: pl.LazyFrame = self.__heads.join(
+            other=pl.read_parquet(pt.BATCH_LINES).lazy(), on=["ID_STATEMENT", "ID_BATCH"], how="inner", validate="1:1", coalesce=True
+        )
+        self.all: pl.LazyFrame = (
             self.raw.select(
-                # id_statement="ID_STATEMENT",
                 id_account="ID_ACCOUNT",
-                # id_statement_int="index",
                 company="STD_COMPANY",
-                # account_type="STD_ACCOUNT_TYPE",
                 account_type="STD_ACCOUNT",
                 account_number="STD_ACCOUNT_NUMBER",
                 account_holder="STD_ACCOUNT_HOLDER",
-                # statement_date="STD_STATEMENT_DATE",
-                # filename="STD_FILENAME",
-                # batch_time="STD_UPDATETIME",
             )
             .group_by("id_account")
             .last()
@@ -200,11 +256,14 @@ class DimAccount:
 
 
 class FactTransaction:
-    def __init__(self) -> None:
-        self.raw: pl.DataFrame = pl.read_parquet(pt.STATEMENT_HEADS).join(
-            other=pl.read_parquet(pt.STATEMENT_LINES), on="ID_STATEMENT", how="inner", validate="1:m", coalesce=True
+    def __init__(self, ID_BATCH: str | None = None) -> None:
+        self.__heads: pl.LazyFrame = (
+            pl.read_parquet(pt.STATEMENT_HEADS).filter((pl.lit(ID_BATCH).is_null()) | (pl.col("ID_BATCH") == pl.lit(ID_BATCH))).lazy()
         )
-        self.all: pl.DataFrame = self.raw.select(
+        self.raw: pl.LazyFrame = self.__heads.join(
+            other=pl.read_parquet(pt.STATEMENT_LINES).lazy(), on="ID_STATEMENT", how="inner", validate="1:m", coalesce=True
+        )
+        self.all: pl.LazyFrame = self.raw.select(
             id_transaction="ID_TRANSACTION",
             id_transaction_int="index_right",
             id_statement="ID_STATEMENT",
@@ -224,7 +283,7 @@ class FactTransaction:
 
 class GapReport:
     def __init__(self):
-        self.raw: pl.DataFrame = (
+        self.raw: pl.LazyFrame = (
             pl.read_parquet(pt.STATEMENT_HEADS)
             .select(
                 "STD_ACCOUNT",
@@ -252,8 +311,8 @@ class GapReport:
                     )
                 )
             )
-        )
-        self.all: pl.DataFrame = self.raw.select(
+        ).lazy()
+        self.all: pl.LazyFrame = self.raw.select(
             account_type="STD_ACCOUNT",
             account_number="STD_ACCOUNT_NUMBER",
             account_holder="STD_ACCOUNT_HOLDER",
@@ -262,7 +321,7 @@ class GapReport:
             closing_balance=pl.col("STD_CLOSING_BALANCE").cast(float),
             gap_flag=pl.when(pl.col("gap")).then(pl.lit("GAP")).otherwise(pl.lit("")),
         )
-        self.gaps: pl.DataFrame = self.all.join(
+        self.gaps: pl.LazyFrame = self.all.join(
             other=self.all.filter(pl.col("gap_flag") == "GAP").select("account_type", "account_number").unique(),
             on=["account_type", "account_number"],
         )
