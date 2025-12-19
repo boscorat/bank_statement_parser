6654f08597383a3cae9d2de20cdf9ea3440164d6
Jason Farrar <stillwaters.technology@gmail.com>
2025-12-19 08:56:26 +0000

    added linux & windows folders to dev.py

--- src/bank_statement_parser/dev.py
+++ src/bank_statement_parser/dev.py
@@ -5,10 +5,13 @@ import polars as pl
 import bank_statement_parser.modules.classes.reports as rp
 import bank_statement_parser.modules.paths as pt
 from bank_statement_parser.modules.classes import statements
-from bank_statement_parser.modules.classes.reports import export_csv, export_db_mssql, export_excel
+from bank_statement_parser.modules.classes.reports import export_csv, export_excel
 
 
 def main():
+    # laptop
+    statements.StatementBatch(Path("/home/boscorat/Downloads/2025"), turbo=False, smart_rename=False)
+    # #windows
     # statements.StatementBatch(Path("C:\\Users\\Admin\\repos\\bsp\\stmts"), turbo=True, smart_rename=False)
 
     last_batch = pl.read_parquet(pt.BATCH_HEADS).select(pl.col("ID_BATCH").last()).item()
@@ -22,11 +25,12 @@ def main():
     # export_excel(pt.EXCEL.joinpath("test_simple.xlsx"), type="simple", ID_BATCH=last_batch)
     # export_excel(pt.EXCEL.joinpath("test.xlsx"), type="full", ID_BATCH=last_batch)
     # export_csv(pt.CSV.joinpath("test_simple.csv"), type="simple", ID_BATCH=last_batch)
-    export_db_mssql(type="full")
+    # export_db_mssql(type="full")
     # export_csv(folder=pt.CSV, type="full", ID_BATCH=last_batch)
 
+    print(pl.read_parquet(pt.STATEMENT_HEADS))
 
-# print(pl.read_parquet(pt.STATEMENT_HEADS))
+
 # print(pl.read_parquet(pt.STATEMENT_LINES))
 
 # db.BatchHeads().truncate()
